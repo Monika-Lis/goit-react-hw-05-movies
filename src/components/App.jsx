6a03fe7596091bd85c4ba6422d59ba6008d59665
Monika-Lis <lis.monikak@gmail.com>
2024-05-12 08:07:10 +0100

    navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { TrendingMovies } from './Sections/Trending';
+import { Layout } from './Sections/Layout';
 
 export const App = () => {
   return (
     <div>
       <Routes>
-        <Route index element={<TrendingMovies />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<TrendingMovies />} />
+        </Route>
       </Routes>
     </div>
   );
